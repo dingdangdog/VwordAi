91d84e501ec1817113607da22baa1263fe16a661
dddx <dingdangdogx@outlook.com>
2024-11-09 15:10:16 +0800

    fix: 样式优化

--- ui/src/views/local.vue
+++ ui/src/views/local.vue
@@ -1,24 +1,66 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div class="p-2">
-    <!-- <h2 class="text-lg">系统设置</h2> -->
-    <div class="m-2 p-2 bg-gray-800 rounded-md">
-      <div class="flex items-center">
-        <h3>本地端口:</h3>
-        <input class="w-16 bg-transparent border-b mx-2" />
-        <button class="px-2 py-1 bg-gray-800 rounded-md hover:bg-gray-700">
+  <div class="h-full p-2 flex flex-col justify-between">
+    <div class="h-12">
+      <div class="p-2 flex items-center bg-gray-800 rounded-md">
+        <h3>本地端口</h3>
+        <input class="w-16 bg-transparent border-b ml-2 focus:outline-none" />
+        <button class="ml-2 px-2 py-1 bg-gray-700 rounded-md hover:bg-gray-600">
           开启本地服务
         </button>
-        <button class="px-2 py-1 bg-red-500 rounded-md hover:bg-red-400">
+        <button class="ml-2 px-2 py-1 bg-red-400 rounded-md hover:bg-red-500">
           停止
         </button>
       </div>
     </div>
 
-    <div class="m-2 p-2 bg-gray-800 rounded-md">
+    <div
+      class="mt-2 px-2 py-1 bg-gray-800 rounded-md flex-1 flex flex-col justify-between"
+      style="height: calc(100% - 4rem)"
+    >
       <h3>调用日志</h3>
-      <div class="w-full h-[80vh] bg-black overflow-y-auto">
+      <div class="w-full flex-1 bg-black overflow-y-auto">
+        <p>xxxx</p>
+        <p>xxxx</p>
+        <p>xxxx</p>
+        <p>xxxx</p>
+        <p>xxxx</p>
+        <p>xxxx</p>
+        <p>xxxx</p>
+        <p>xxxx</p>
+        <p>xxxx</p>
+        <p>xxxx</p>
+        <p>xxxx</p>
+        <p>xxxx</p>
+        <p>xxxx</p>
+        <p>xxxx</p>
+        <p>xxxx</p>
+        <p>xxxx</p>
+        <p>xxxx</p>
+        <p>xxxx</p>
+        <p>xxxx</p>
+        <p>xxxx</p>
+        <p>xxxx</p>
+        <p>xxxx</p>
+        <p>xxxx</p>
+        <p>xxxx</p>
+        <p>xxxx</p>
+        <p>xxxx</p>
+        <p>xxxx</p>
+        <p>xxxx</p>
+        <p>xxxx</p>
+        <p>xxxx</p>
+        <p>xxxx</p>
+        <p>xxxx</p>
+        <p>xxxx</p>
+        <p>xxxx</p>
+        <p>xxxx</p>
+        <p>xxxx</p>
+        <p>xxxx</p>
+        <p>xxxx</p>
+        <p>xxxx</p>
+        <p>xxxx</p>
         <p>xxxx</p>
       </div>
     </div>
