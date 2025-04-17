1be348a52acac5b8455f86773877e19103e090fc
dddx <dingdangdogx@outlook.com>
2025-04-17 11:56:18 +0800

    release: v0.0.5

--- ui/src/stores/appConfig.ts
+++ ui/src/stores/appConfig.ts
@@ -7,7 +7,7 @@ export const appConfig = {
   // 基本信息
   name: "文声AI",
   nameEn: "VwordAi",
-  version: "0.0.4",
+  version: "0.0.5",
   releaseDate: "2025年4月16日",
   copyright: "© 2025 VwordAi.com 保留所有权利。",
   
