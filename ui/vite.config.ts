e31ac396806a535df49247392f65f1dfda68e802
dddx <dingdangdogx@outlook.com>
2025-04-17 11:44:31 +0800

    chore: clean up Vite config

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -1,17 +1,13 @@
 import { fileURLToPath, URL } from "node:url";
-
-import vue from "@vitejs/plugin-vue";
 import { defineConfig, loadEnv } from "vite";
+import vue from "@vitejs/plugin-vue";
 // 读取环境变量
 const env = loadEnv("", process.cwd());
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: env.VITE_APP_BASE_URL || "/",
-  plugins: [
-    vue(),
-    // vueDevTools(),
-  ],
+  plugins: [vue()],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
