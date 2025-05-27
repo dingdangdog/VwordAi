e18f6df3680e112f2eb1a169c9c3152a6f5a8693
dddx <dingdangdogx@outlook.com>
2025-05-27 16:15:18 +0800

    feat: Bump version to 1.0.3 in package.json and appConfig.ts for vwordai and vwordai-ui

--- ui/src/stores/appConfig.ts
+++ ui/src/stores/appConfig.ts
@@ -7,7 +7,7 @@ export const appConfig = {
   // 基本信息
   name: "文声AI",
   nameEn: "VwordAi",
-  version: "0.1.3",
+  version: "1.0.3",
   releaseDate: "2025年5月9日",
   copyright: "©2025 VwordAi.com 保留所有权利。",
 
